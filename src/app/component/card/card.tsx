366a754c52d9f6565cfaaa863c5feecb653bb4c4
prabakaran <praba104016@gmail.com>
2025-02-22 14:03:12 +0530

    initial

--- src/app/component/card/card.tsx
+++ src/app/component/card/card.tsx
@@ -1,11 +1,15 @@
 import './card.css'
 import Image from 'next/image';
-export default function Home(books ) {
+interface BooksProps {
+  books: string;
+}
+
+export default function Home({ books }: BooksProps) {
     return (
       <>
         <div className="card">
           <div className="card-container">
-            <Image src={books.books} alt="Avatar" className="card-image" />
+            <Image src={books} alt="Avatar" className="card-image" />
            
               <p>Book Name</p>
               <button className='btn'> Add To Cart +</button>
