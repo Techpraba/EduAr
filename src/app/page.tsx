da066ad20134ee89f847f587b326ab0baa9cee6b
prabakaran <praba104016@gmail.com>
2025-02-26 13:58:08 +0530

    sec page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import './home.css'
 import pic from './asserts/book-lover-concept-illustration.png'
 import book1 from './asserts/books/6920933-removebg-preview.png'
 
+import { FaBookReader ,  FaHandHoldingHeart,FaRecycle} from "react-icons/fa";
+
+
 
 
 function Home() {
@@ -16,7 +19,7 @@ function Home() {
             <p>Learning Platform</p>
             <h1 className="booking">Shop <br></br>Book<br></br> Online</h1>
           <p>Get the best deals on your favorite brands</p> 
-          <button>Shop Now</button>
+          <button className="homebtn">Shop Now</button>
         </div>
         <div className="centerimg">
           <Image src={pic} alt="Book Lover Concept Illustration" className="centerpic" />
@@ -30,7 +33,39 @@ function Home() {
           
         </div>
         
-    </div>
+      </div>
+      {/* Second Part of the website */}
+      <div className="secPartOverall">
+        <div className="leftside lColor">
+          <h1 className="booking">Welcome to Self Book</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A nemo sint voluptate nobis exercitationem nihil commodi accusantium earum aliquam, ut voluptatibus minus fuga asperiores. Non dicta sunt minus tempore vel?</p>
+          <button className="homebtn">Shop</button>
+          <div className="homeicons">
+           <div>
+              <FaBookReader />
+              <h5 className="bookHeadline">Buying a book ? <br />Make Us A Offer</h5>
+              <p  className="bookHeadline bookpara">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, fugit!</p>
+            </div>
+            <div>
+            <FaHandHoldingHeart />
+              <h5 className="bookHeadline">Buying a book ? <br />Make Us A Offer</h5>
+              <p  className="bookHeadline bookpara">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, fugit!</p>
+            </div>
+            <div>
+            <FaRecycle />
+              <h5 className="bookHeadline">Buying a book ? <br />Make Us A Offer</h5>
+              <p  className="bookHeadline bookpara">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, fugit!</p>
+            </div>
+        
+        
+       
+          
+          </div>
+        </div>
+        <div className="rightside rColor">
+          <Image src={pic} alt="Book Lover Concept Illustration" className="centerpic" />
+        </div>
+      </div>
     </>
   );
 }
